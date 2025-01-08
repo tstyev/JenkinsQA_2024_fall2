3e4cf4d7db752d2cc206e5f235ae5416023a766e
YevgenyN <evgenii.tst@gmail.com>
2025-01-08 04:33:45 +0300

    Test

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -1,5 +1,8 @@
 package school.redrover.runner;
 
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
@@ -7,6 +10,7 @@ import org.testng.annotations.*;
 import school.redrover.runner.order.OrderForTests;
 import school.redrover.runner.order.OrderUtils;
 
+import java.io.ByteArrayInputStream;
 import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.Arrays;
@@ -105,6 +109,14 @@ public abstract class BaseTest {
     protected void afterMethod(Method method, ITestResult testResult) {
         if (!testResult.isSuccess() && ProjectUtils.isServerRun()) {
             ProjectUtils.takeScreenshot(getDriver(), testResult.getTestClass().getRealClass().getSimpleName(), testResult.getName());
+        }
+
+        if (!testResult.isSuccess()) {
+            Allure.addAttachment(
+                    "screenshot.png",
+                    "image/png",
+                    new ByteArrayInputStream(((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES)),
+                    "png");
         }
 
         if (methodsOrder.isGroupFinished(method) && (ProjectUtils.isServerRun() || testResult.isSuccess() || ProjectUtils.closeBrowserIfError())) {
